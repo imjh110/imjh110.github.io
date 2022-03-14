c6eb1db7a7735725407cfc9223675073d15f82dc
imjh110 <imjh110@naver.com>
2022-03-14 17:44:55 +0900

    add minimal-mistake theme

---
+++ _posts/2022-03-14-hi-this-is-my-new-post.md
@@ -0,0 +1,5 @@
+---
+title: My name is Junhyeok Im. Nice to meet you.
+---
+
+my new post!
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "My name is Junhyeok Im. Nice to meet you.",
+        "excerpt":"my new post!  ","categories": [],
+        "tags": [],
+        "url": "/hi-this-is-my-new-post/",
+        "teaser": null
+      }]
